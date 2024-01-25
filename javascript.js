4afc7d0b67ff9b94ee948126b05d6e2bea966da4
Kira <Kira@blanks-MacBook-Pro.local>
2024-01-24 22:12:29 -0500

    addition of HTML webform on contact page and general style improvements

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-alert("Last Updated Jan 21 2024")
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
+
+// const e = document.getElementsByTagName('footer')[0];
+
+// e.innerHTML = 'Last updated Jan 23 2024'
+
+// alert("Last Updated Jan 21 2024")
